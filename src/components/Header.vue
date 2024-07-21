55529c8208034f6e5f015de2fdd1e9a8185f67bb
Joojinsung <dev.jinsung1017@gmail.com>
2024-07-21 17:30:08 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Search',
+          href: '/',
+        },
+        {
+          name: 'Movie',
+          href: '/Movie',
+        },{
+          name: 'About',
+          href: '/About',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<template>
+  <header>
+    <div class="nav">
+      <div class="nav-item">
+        <RouterLink class="nav-link" to="">
+          About
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
