3562cace52c81e93d5a73054c21837f26db56f10
Joojinsung <dev.jinsung1017@gmail.com>
2024-07-22 21:44:10 +0900

    Search - 필터 목록 구현

---
+++ src/components/Search.vue
@@ -0,0 +1,99 @@
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'series', 'episode']
+        },
+        {
+          name: 'number',
+          items: [10, 20, 30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years = []
+            const thisYear = new Date().getFullYear();
+            for (let i = thisYear; i >= 1985; i -= 1) {
+              years.push(i)
+            }
+            return years;
+          })()
+        },
+      ]
+    }
+  },
+  methods:{
+    apply() {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <input
+        @keyup.enter="apply"
+        type="text"
+        class="form-control"
+        v-model="title"
+        placeholder="Search for Movies, TV Shows, Person......">
+    <div class="selects">
+      <select
+          v-for="filter in filters"
+          :key="filter.name"
+          v-model="$data[filter.name]"
+          class="form-select">
+        <option v-if="filter.name === 'year'" value="">
+          All Years
+        </option>
+        <option
+            v-for="item in filter.items" key="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+    <button class="btn btn-primary" @click="apply">Apple</button>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+
+  > * {
+    margin-right: 10px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+
+  .selects {
+    display: flex;
+
+    select {
+      width: 120px;
+      margin-right: 10px;
+
+      &:last-child {
+        margin-right: 10px;
+      }
+    }
+  }
+  .btn {
+    width: 120px;
+    height: 50px;
+    font-weight: 700;
+    flex-shrink: 0;
+  }
+}
+</style>
